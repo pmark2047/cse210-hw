799ab032683702dd7609b8cbe1747c57798aa296
pmark2047 <peytonmarkus24@gmail.com>
2024-01-13 15:01:32 -0700

    Inserted name on line 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+
+        Console.Write("What's Ur Age? ");
+
+        var ageString = Console.ReadLine();
+        Console.WriteLine($"My age is {ageString}.");
+
+        int age = int.Parse(ageString);
+        if (age < 18)
+        {
+            Console.WriteLine("You're a minor");
+        }
     }
 }
