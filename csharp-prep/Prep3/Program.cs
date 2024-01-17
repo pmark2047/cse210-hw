3c279e9c289e8cc4e4795235bacc1356faa3d916
pmark2047 <peytonmarkus24@gmail.com>
2024-01-17 11:00:54 -0700

    Guess number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Formats.Asn1;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Please give a positive integer: ");
+
+        int maxInt = int.Parse(Console.ReadLine());
+
+        float guess = -5;
+
+        Random randomGenerator = new Random();
+        int answer = randomGenerator.Next(1, maxInt);
+
+        do
+        {
+            Console.Write($"Pick a number between 1 and {maxInt}: ");
+
+            guess = float.Parse(Console.ReadLine());
+
+            if (guess < answer)
+            {
+                Console.WriteLine("     Too low!");
+            }
+            else if (guess > answer)
+            {
+                Console.WriteLine("     Too high!");
+            }
+            else
+            {
+                Console.Write("     Correct!");
+            }
+
+        } while (guess != answer);
+
+
     }
 }
